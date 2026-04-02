c6127427d6675048e0597e3c4332ed013fa2897b
Farhan6Ishraq <119164785+Farhan6Ishraq@users.noreply.github.com>
2026-04-02 21:01:04 +0600

    Added signIn, signUp backend logic through betterAuth, set up database

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,10 +1,47 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Card, CardTitle, CardDescription, CardHeader, CardContent, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { signIn } from "@/lib/auth/auth-client";
 import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function SignIn() {
+    const router = useRouter();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    async function handleSubmit(e: React.FormEvent) {
+        e.preventDefault();
+
+        setError("");
+        setLoading(true);
+
+        try {
+            const result = await signIn.email({
+                email,
+                password
+            })
+
+            if(result.error) {
+                setError(result.error.message ?? "Something went wrong.");
+            } else {
+                router.push("./dashboard");
+            }
+        } catch(err) {
+            setError("An unexpected error occured.");
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return (
         <div className="flex min-h-[calc(100vh-64px)] justify-center items-center p-4">
             <Card>
@@ -12,20 +49,29 @@ export default function SignIn() {
                     <CardTitle>Sign In</CardTitle>
                     <CardDescription>Welcome Back!</CardDescription>
                 </CardHeader>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <CardContent>
+                        {
+                        error && (<div className="mb-4 border rounded-sm p-2 text-center bg-destructive opacity-75 text-white font-medium">{error}</div>)
+                        }
                         <div>
                             <Label htmlFor="email">Email:</Label>
-                            <Input id="email" type="email" placeholder="JohnDoe@example.com" required></Input>
+                            <Input id="email" type="email" placeholder="JohnDoe@example.com" value={email} required
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                            }}></Input>
                         </div>
                         <div>
                             <Label htmlFor="password">Password:</Label>
-                            <Input id="password" type="password" placeholder="********" minLength={8} required></Input>
+                            <Input id="password" type="password" placeholder="********" minLength={8} value={password} required
+                            onChange={(e) => {
+                                setPassword(e.target.value);
+                            }}></Input>
                         </div>
                     </CardContent>
                     <CardFooter className="flex flex-col">
-                        <Button type="submit">Sign In</Button>
-                        <p>Don't have an account? <Link href="/sign-in" className="text-primary hover:underline">Sign Up</Link></p>
+                        <Button type="submit" disabled={loading}>{loading ? "Logging In" : "Sign In"}</Button>
+                        <p>Don't have an account? <Link href="/sign-up" className="text-primary hover:underline">Sign Up</Link></p>
                     </CardFooter>
                 </form>
             </Card>
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -5,15 +5,47 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { useState } from "react";
+import { signUp } from "@/lib/auth/auth-client"
+import { useRouter } from "next/navigation";
 
 export default function SignUp() {
+    const router = useRouter();
+
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
     // checker for minimum password length
     const [isDirty, setIsDirty] = useState(false);
     const isMinPassCheck = isDirty && password.length > 0 && password.length <= 8;
+
+    async function handleSubmit(e: React.FormEvent) {
+        e.preventDefault();
+
+        setError("");
+        setLoading(true);
+
+        try {
+            const result = await signUp.email({
+                name,
+                email,
+                password,
+            });
+
+            if(result.error) {
+                setError(result.error.message ?? "Error");
+            } else {
+                router.push("/dashboard");
+            }
+        } catch(err) {
+            setError("An unexpected error occured.");
+        } finally {
+            setLoading(false);
+        }
+    }
 
     return (
         <div className="flex min-h-[calc(100vh-64px)] justify-center items-center p-4">
@@ -22,8 +54,11 @@ export default function SignUp() {
                     <CardTitle>Sign Up</CardTitle>
                     <CardDescription>Make an Account to start Organizing!</CardDescription>
                 </CardHeader>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <CardContent>
+                        { error && (
+                            <div className="mb-4 border rounded-sm p-2 text-center bg-destructive opacity-75 text-white font-medium">{error}</div>
+                        )}
                         <div>
                             <Label htmlFor="name">Name:</Label>
                             <Input id="name" value={name} type="text" placeholder="John Doe" onChange={(e) => {
@@ -47,7 +82,7 @@ export default function SignUp() {
                         </div>
                     </CardContent>
                     <CardFooter className="flex flex-col">
-                        <Button type="submit">Sign Up</Button>
+                        <Button type="submit" disabled={loading}>{loading ? "Creating an Account" : "Sign Up"}</Button>
                         <p>Already have an account? <Link href="/sign-in" className="text-primary hover:underline">Sign In</Link></p>
                     </CardFooter>
                 </form>
