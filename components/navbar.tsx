516acc3e3a6489e0195105aed3ed13dac0833092
Farhan6Ishraq <119164785+Farhan6Ishraq@users.noreply.github.com>
2026-04-03 01:42:36 +0600

    Added a signout option and dynamic navbar UI

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import { Briefcase, Ghost } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel } from "./ui/dropdown-menu";
+import { DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import SignOutButton from "./sign-out-btn";
+import { useSession } from "@/lib/auth/auth-client";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
+    const { data:session } = useSession();
+    const router = useRouter();
     return (
         <nav className="border-b border-gray-200 bg-white">
             <div className="flex container mx-auto h-16 items-center px-4 justify-between">
@@ -11,12 +21,36 @@ export default function Navbar() {
                     Job Organizer
                 </Link>
                 <div className="flex items-center gap-4">
-                    <Link href="/sign-up">
+                    {session?.user ? (<>
+                        <Link href="/dashboard">Dashboard</Link>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger>
+                                <Button variant="ghost">
+                                    <Avatar>
+                                        <AvatarFallback className="bg-primary text-white">
+                                            {session.user.name[0].toUpperCase()}
+                                        </AvatarFallback>
+                                    </Avatar>
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                                <DropdownMenuLabel>
+                                    <div>
+                                        <p>{session.user.name}</p>
+                                        <p>{session.user.email}</p>
+                                    </div>
+                                </DropdownMenuLabel>
+                                <SignOutButton />
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </>) : ( <>
+                        <Link href="/sign-up">
                         <Button className="bg-primary hover:bg-primary/90">Sign Up for free</Button>
                     </Link>
                     <Link href="/sign-in">
                         <Button variant="ghost" className="text-gray-700 hover:text-black">Log in</Button>
                     </Link>
+                    </>)}
                 </div>
             </div>
         </nav>
